381dfb896fd501c28c8007dd92a9694a4d606b41
tiantingrui <976912095@qq.com>
2023-02-25 11:23:18 +0800

    ci: :ferris_wheel: cz-git add

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,10 +1,10 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['feat', 'fix', 'ci', 'pref', 'refactor', 'style', 'test', 'chore', 'build', 'revert', 'docs']
-    ]
-  }
+  extends: ['@commitlint/config-conventional']
+  // rules: {
+  //   'type-enum': [
+  //     2,
+  //     'always',
+  //     ['feat', 'fix', 'ci', 'pref', 'refactor', 'style', 'test', 'chore', 'build', 'revert', 'docs']
+  //   ]
+  // }
 }
